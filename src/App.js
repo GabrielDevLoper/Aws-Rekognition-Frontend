31c8969f29704e1b12dd7c1397ecbe2d21b14538
gabriel <gabriel.devloper@gmail.com>
2019-11-15 23:07:33 -0300

    Implementando WemCam e faceapi.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,88 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
-import api from './services/api'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as faceapi from 'face-api.js';
+const Webcam = require('./components/webCam');
 
-function App() {
-  function handleSubmit(event){
-    event.preventDefault();
-    
+//const video = document.getElementById('video');
+
+const MODEL_URL = '/models'
+const minConfidence = 0.6
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.fullFaceDescriptions = null;
+    this.canvas = React.createRef();
+    this.canvasPicWebCam = React.createRef();
   }
 
 
+  async componentDidMount(){
+    await this.loadModels();
+    const testImageHTML = document.getElementById('test');
+    this.drawHTMLImage(this.canvas.current,testImageHTML,296,296);
+    await this.getFullFaceDescription(this.canvas.current);
+    this.drawDescription(this.canvas.current);
+  }
 
-  return (
-    <div className="container">
-     <div className="content">
-       <form onSubmit={handleSubmit}>
-          <button className="btn"type="submit">Cadastrar Clientes</button>
-       </form>
-       <form>
-       <button className="btn" type="submit">Reconhecimento Facial</button>
-       </form>
-     </div>
-     <div>
-     </div>
-    </div>
+  async loadModels(){
+    await faceapi.loadFaceDetectionModel(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceRecognitionModel(MODEL_URL);
+  }
+
+  /*
+  async startVideo(){
+   await navigator.getUserMedia({ video: {} },
+      stream => video.srcObject = stream,
+      err => console.error(err));
     
-    
-  );
+  }
+  */
+ getFullFaceDescription = async (canvas) => {
+  console.log(canvas);
+  this.fullFaceDescriptions = await faceapi.allFaces(canvas, minConfidence);
+  console.log(this.fullFaceDescriptions);
+  }
+
+  drawDescription = (canvas) => {
+    this.fullFaceDescriptions.forEach((fd, i) => {
+      faceapi.drawLandmarks(canvas, fd.landmarks, { drawLines: true })
+    });
+  }
+
+  drawHTMLImage(canvas,image,width,height){
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image,0,0,width,height);
+  }
+
+  landmarkWebCamPicture = (picture) => {
+    const ctx = this.canvasPicWebCam.current.getContext("2d");
+    var image = new Image();
+    image.onload = async () => {
+      ctx.drawImage(image,0,0);
+      await this.getFullFaceDescription(this.canvasPicWebCam.current);
+      this.drawDescription(this.canvasPicWebCam.current);
+    };
+    image.src = picture;
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 100 }}>
+            <canvas ref={this.canvas} width={296} height={296} />
+          </div>
+          <Webcam landmarkPicture={this.landmarkWebCamPicture} />
+          <canvas ref={this.canvasPicWebCam} width={350} height={350} /> 
+      </div>
+    );
+  }
 }
 
-export default App;
+
+ 
+
+
+
